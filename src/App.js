cf46cf9ff7790026d52246742a67632eac163554
Ayush <ayushchaurasiya78@gmail.com>
2023-06-03 10:47:41 +0530

    sidebar, index.html, app.js modified

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
         dispatch(logout())
       }
     })
-  }, [])
+  }, [dispatch])
 
 
   return (
@@ -45,8 +45,6 @@ function App() {
           <Widgets />
       </div>
       )}
-      
-      
     </div>
   );
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -19,9 +19,9 @@ function Sidebar() {
     <div className='sidebar'>
         <div className="sidebar-top">
             <img src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=100&q=80" alt="" />
-            <Avatar src={user.photoUrl} className='sidebar-avatar'>{user.displayName[0]}</Avatar>
-            <h2>{user.displayName}</h2>
-            <h4>{user.email}</h4>
+            <Avatar src={user?.photoUrl} className='sidebar-avatar'></Avatar>
+            <h2>{user?.displayName}</h2>
+            <h4>{user?.email}</h4>
         </div>
 
         <div className="sidebar-stats">
